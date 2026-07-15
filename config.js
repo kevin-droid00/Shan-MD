fe0294f37fc01a80b3f07ed71657d951858a0eb5
kevin-droid00 <kevinjgjy@gmail.com>
2026-07-15 19:05:56 +0530

    Update config.js

--- config.js
+++ config.js
@@ -4,20 +4,22 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 
-//gg
 module.exports = {
-SESSION_ID:'6ZknzSKR#KGsNXinavE78XWj2YqLSOW5DUrOS4S0uJ0zyXCBmDig', // methanata ube id eka daganim
+SESSION_ID: '6ZknzSKR#KGsNXinavE78XWj2YqLSOW5DUrOS4S0uJ0zyXCBmDig', // ඔයාගේ Session ID එක අනිවාර්යයෙන්ම මෙතනට දාන්න.
+
+PREFIX: process.env.PREFIX === undefined ? '.' : process.env.PREFIX, // Prefix එක '.' ලෙස auto set වේ.
+MODE: process.env.MODE === undefined ? 'public' : process.env.MODE, // Bot එක public වැඩ කිරීමට.
 
 ANTI_DELETE: process.env.ANTI_DELETE === undefined ? 'true' : process.env.ANTI_DELETE, 
-MV_BLOCK: process.env. MV_BLOCK === undefined ? 'true' : process.env. MV_BLOCK,    
+MV_BLOCK: process.env.MV_BLOCK === undefined ? 'true' : process.env.MV_BLOCK, // Space එක අයින් කරන ලදී   
 ANTI_LINK: process.env.ANTI_LINK === undefined ? 'true' : process.env.ANTI_LINK, 
 SEEDR_MAIL: '',
 SEEDR_PASSWORD: '',
-SUDO: '',//
+SUDO: '94711726564', // ඔයාගේ නම්බර් එක මෙතනටත් දාන්න
 DB_NAME: 'Shan',
 LANG: 'SI',
 OWNER_NUMBER: '94711726564',
 LOGO: process.env.LOGO === undefined ? 'https://files.catbox.moe/nsv9gv.jpg' : process.env.LOGO
 
 };
-//GITHUB_AUTH_TdOKEN: 'ouvnI0xSDsmfWA1filVxx.SZ0vJGYkjlC5VX54U0e10',
+    
